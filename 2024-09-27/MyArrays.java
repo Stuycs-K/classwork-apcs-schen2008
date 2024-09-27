2d329c28d223f631dd31cd1d8ee0a501dd9ad7cc
Sean Chen <schen200826@gmail.com>
2024-09-27 10:56:27 -0400

    finished concatArray

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,20 +1,21 @@
 public class MyArrays{
-  public static Sttring arrayToString(int[] nums)
+  public static String arrayToString(int[] nums)
   {
     String result = "[";
     for(int i = 0; i < nums.length; i++)
     {
       if(i == nums.length-1)
       {
-        result += nums[i]
+        result += nums[i];
       }
       else
       {
       result += nums[i] + ",";
       }
     }
-    return result + "]"
+    return result + "]";
   }
+
   public static int[] returnCopy(int[] ary)
   {
     int[] result = new int[ary.length];
@@ -24,7 +25,18 @@ public class MyArrays{
     }
     return result;
   }
-  
+
+  public static int[] concatArray(int[] ary1, int[] ary2){
+    int[] result = new int[ary1.length + ary2.length];
+    int i = 0;
+    for(i = 0; i<ary1.length;i++){
+      result[i] = ary1[i];
+    }
+    for(int x = 0; x < ary2.length; x++){
+      result[i] = ary1[x];
+    }
+    return result;
+  }
   public static void main(String[] args)
   {
 
