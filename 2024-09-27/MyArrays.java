93f43383adb95d2c6faaf74b47d17515c1b6831b
Sean Chen <schen200826@gmail.com>
2024-09-29 22:32:24 -0400

    finishing

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,5 +1,5 @@
 public class MyArrays{
-  public static String arrayToString(int[] nums)
+  public static String aryToString(int[] nums)
   {
     String result = "[";
     for(int i = 0; i < nums.length; i++)
@@ -10,7 +10,7 @@ public class MyArrays{
       }
       else
       {
-      result += nums[i] + ",";
+      result += nums[i] + ", ";
       }
     }
     return result + "]";
@@ -26,22 +26,51 @@ public class MyArrays{
     return result;
   }
   
-  System.out.println()
-
   public static int[] concatArray(int[] ary1, int[] ary2){
     int[] result = new int[ary1.length + ary2.length];
-    int i = 0;
-    for(i = 0; i<ary1.length;i++){
-      result[i] = ary1[i];
+    int track = 0;
+    for(int i = 0; i<ary1.length;i++){
+      result[track] = ary1[i];
+      track++;
     }
     for(int x = 0; x < ary2.length; x++){
-      result[i] = ary1[x];
+      result[track] = ary1[x];
+      track++;
     }
     return result;
   }
   public static void main(String[] args)
   {
-    System.out.println(arrayToString())
+    int[] arr1 = {1,2,3,4,5};
+    int[] arr2 = {};
+    int[] arr3 = {10000,1000,10,0,-5};
+    int[] arr4 = {0,0,0,0,0,0,0,1};
+    int[] arr5 = {-5,-24,1,55,4,5,15};
+
+  System.out.println("ARYTOSTRING");
+
+  System.out.println(aryToString(arr1));
+  System.out.println(aryToString(arr2));
+  System.out.println(aryToString(arr3));
+  System.out.println(aryToString(arr4));
+  System.out.println(aryToString(arr5));
+
+System.out.println("RETURN COPY");
+
+  System.out.println("expected: " + aryToString(arr1) + " result: " + aryToString(returnCopy(arr1)));
+  System.out.println("expected: " + aryToString(arr2) + " result: " + aryToString(returnCopy(arr2)));
+  System.out.println("expected: " + aryToString(arr3) + " result: " + aryToString(returnCopy(arr3)));
+  System.out.println("expected: " + aryToString(arr4) + " result: " + aryToString(returnCopy(arr4)));
+  System.out.println("expected: " + aryToString(arr5) + " result: " + aryToString(returnCopy(arr5)));
+
+ System.out.println("CONCAT ARRAY");
+
+  System.out.println(aryToString(concatArray(arr1, arr2)));
+  System.out.println(aryToString(concatArray(arr1, arr3)));
+  System.out.println(aryToString(concatArray(arr1, arr4)));
+  System.out.println(aryToString(concatArray(arr1, arr5)));
+  System.out.println(aryToString(concatArray(arr2, arr3)));
+  System.out.println(aryToString(concatArray(arr2, arr4)));
 
   }
 }
