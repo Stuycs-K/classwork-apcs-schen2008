1d38ec291a21ddc397144a6c9cbbe322002953d5
Sean Chen <schen200826@gmail.com>
2024-10-15 22:16:35 -0400

    added the rest of the test cases before writing equilateral trinalge

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -12,11 +12,17 @@ public class Demo{
     Point p1 = new Point(1,1);
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
+    Point p4 = new Point(10, -10);
     Point p1Copy = new Point(p1);
+    Point p2Copy = new Point(p2);
     System.out.println( p3);
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
     System.out.println( p1Copy);
+    System.out.println(p2Copy);
+    System.out.println(Point.distance(p1,p4));
+    System.out.println(p4.distanceTo(p1));
+
   }
 }
