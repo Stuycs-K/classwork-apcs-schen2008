d16cbfa83c938489f52c8085764094f48782443c
Sean Chen <schen200826@gmail.com>
2024-11-05 17:43:59 -0500

    wrote while loop that prints all lines with { and tested

--- 10-31-Advent/ReadFile.java
+++ 10-31-Advent/ReadFile.java
@@ -5,20 +5,16 @@ import java.util.Scanner;
 public class ReadFile {
 
   public static void main(String[] args) {
-    int counter = 0;
     //2 Opening a file requires a try/catch
     try {
       File file = new File("ReadFile.java");//1
       Scanner input = new Scanner(file);
       while(input.hasNextLine()){
-        if (input.nextLine().contains("{")) {
-          //make a variable adn then save it and then print that
-          System.out.println(input.nextLine());
+        String nline = input.nextLine();
+        if (nline.contains("{")) {
+          //make a variable and then save it and then print that
+          System.out.println(nline);
         }
-        else {
-          input.nextLine();
-        }
-        counter++;
       }
       //CODE THAT SCANS THE FILE.
       input.close();//releases the file from your program
