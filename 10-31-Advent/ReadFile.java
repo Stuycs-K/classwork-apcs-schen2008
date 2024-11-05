39f878b07f6268b0ae5ffb663cedf75421f51723
Sean Chen <schen200826@gmail.com>
2024-11-05 17:33:51 -0500

    copied work with partner into ReadFile.java

--- 10-31-Advent/ReadFile.java
+++ 10-31-Advent/ReadFile.java
@@ -1,4 +1,3 @@
-//1 you need all three imports.
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -6,12 +5,20 @@ import java.util.Scanner;
 public class ReadFile {
 
   public static void main(String[] args) {
+    int counter = 0;
     //2 Opening a file requires a try/catch
     try {
-      File file = new File("TriangleTester.java");//1
+      File file = new File("ReadFile.java");//1
       Scanner input = new Scanner(file);
       while(input.hasNextLine()){
-        System
+        if (input.nextLine().contains("{")) {
+          //make a variable adn then save it and then print that
+          System.out.println(input.nextLine());
+        }
+        else {
+          input.nextLine();
+        }
+        counter++;
       }
       //CODE THAT SCANS THE FILE.
       input.close();//releases the file from your program
