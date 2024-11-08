cc1a9b1e88e3a85a130ebd61b17db79db8a791df
Sean Chen <schen200826@gmail.com>
2024-11-07 21:20:48 -0500

    tested all variables speaking

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,8 +1,8 @@
 // We overrode the getName() method to return "The Mighty" + name
 // instead of just name. We changed getName() for Bird.java but Animal.java
 //still has its getName(), which we can call with super.getName().  
-// Assigning a animal to a bird does not work
-// since bird is an extension of animal
+// Assigning a animal to a bird does not work since bird is an extension of animal,
+//  animal doesn't have all the necessary parameters to be a bird 
 //
 public class Driver{
     public static void main(String[] args){
@@ -18,5 +18,8 @@ public class Driver{
       Bird b1 = new Bird("chirp", 9, "songbird", 2.0, "pink");
       // Bird b2 = new Animal("ribbit", 5, "frog");
       Animal a2 = new Bird("quack", 7, "duck", 8.0, "golden");
+      a1.speak();
+      b1.speak();
+      a2.speak();
     }
   }
