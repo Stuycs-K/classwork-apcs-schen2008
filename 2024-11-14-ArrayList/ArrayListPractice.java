e4e522afe6ee37ef5af6e8fae88ec245147ad7eb
Sean Chen <schen200826@gmail.com>
2024-11-14 10:58:46 -0500

    saving before class end

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -17,4 +17,19 @@ public class ArrayListPractice{
     }
     return result;
   }
+
+  public static void replaceEmpty( ArrayList<String> original){
+//Modify the ArrayList such that it has all of the empty strings are
+//replaced with the word "Empty".
+  }
+
+  // public static ArrayList<String> makeReversedList( ArrayList<String> original){
+  //   //return a new ArrayList that is in the reversed order of the original.
+  // }
+  //
+  // public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+  //   //return a new ArrayList that has all values of a and b in alternating order that is:
+  //   //a[0], b[0], a[1], b[1]...
+  //   //If one list is longer than the other, just attach the remaining values to the end.
+  // }
 }
---
+++ 2024-11-14-ArrayList/Driver.java
@@ -0,0 +1,14 @@
+// import java.util.ArrayList;
+
+
+public class Driver{
+  public static void main(String[] args){
+    System.out.println(ArrayListPractice.createRandomArray(10));
+    System.out.println(ArrayListPractice.createRandomArray((int)(Math.random()*10)));
+    ArrayListPractice.createRandomArray(200000);
+
+    // ArrayList<String> test =  ArrayListPractice.createRandomArray(5);
+
+    }
+  }
+}
