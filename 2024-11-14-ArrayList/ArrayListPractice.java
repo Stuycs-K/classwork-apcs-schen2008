cf1a1c06ab4037330999596626e90a8a68ad061d
Sean Chen <schen200826@gmail.com>
2024-11-17 17:49:04 -0500

    wrote and tested makeReversedList

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -4,7 +4,7 @@ public class ArrayListPractice{
   // Write a method public static ArrayList<String>createRandomArray(int size)
   // Create and return an ArrayList of the specified size that is randomly filled. Fill it with strings that look like the integers from 0 to 10 or similar,
   // but make every 0 turn into the empty string "". Do not make very long numbers that are difficult to read. You shoudl use Math.random()
-  public static ArrayList<String> createRandomArray (int size){
+  public static ArrayList<String> createRandomArray(int size){
     ArrayList<String> result = new ArrayList<String>();
     for (int i = 0; i < size; i++){
       int x = (int)(11 * Math.random()); //this takes away very small chance that we get 11
@@ -28,11 +28,17 @@ public class ArrayListPractice{
     }
 
   }
-
+  public static ArrayList<String> makeReversedList( ArrayList<String> original){
+    //return a new ArrayList that is in the reversed order of the original.
+    ArrayList<String> result = new ArrayList<String>();
+    for(int i = original.size()-1; i >= 0; i--){
+     result.add(original.get(i));
+    }
+    return result;
+   }
 }
-  // public static ArrayList<String> makeReversedList( ArrayList<String> original){
-  //   //return a new ArrayList that is in the reversed order of the original.
-  // }
+
+  
   //
   // public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
   //   //return a new ArrayList that has all values of a and b in alternating order that is:
--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -1,7 +1,4 @@
-// import java.util.ArrayList;
-
 import java.util.ArrayList;
-
 public class Driver{
   public static void main(String[] args){
     System.out.println(ArrayListPractice.createRandomArray(10));
@@ -10,10 +7,7 @@ public class Driver{
     System.out.println(test1);
     ArrayListPractice.replaceEmpty(test1);
     System.out.println(test1);
-
-
-
+    System.out.println(ArrayListPractice.makeReversedList(test1));
     // ArrayList<String> test =  ArrayListPractice.createRandomArray(5);
-
   }
 }
