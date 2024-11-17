82ba44916cc4a5812b1e343f407f4e877e69e43d
Sean Chen <schen200826@gmail.com>
2024-11-17 15:29:15 -0500

    wrote replaceEmpty()

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -19,10 +19,17 @@ public class ArrayListPractice{
   }
 
   public static void replaceEmpty( ArrayList<String> original){
-//Modify the ArrayList such that it has all of the empty strings are
-//replaced with the word "Empty".
+  //Modify the ArrayList such that it has all of the empty strings are
+  //replaced with the word "Empty".
+    for(int i = 0; i < original.size(); i++){
+      if(original.get(i).equals("")){
+        original.set(i,"Empty");
+      }
+    }
+
   }
 
+}
   // public static ArrayList<String> makeReversedList( ArrayList<String> original){
   //   //return a new ArrayList that is in the reversed order of the original.
   // }
@@ -32,4 +39,3 @@ public class ArrayListPractice{
   //   //a[0], b[0], a[1], b[1]...
   //   //If one list is longer than the other, just attach the remaining values to the end.
   // }
-}
