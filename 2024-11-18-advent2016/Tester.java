bf68ee8c3b2b6eea25ba61d1dd20c1a19a0c77cc
Sean Chen <schen200826@gmail.com>
2024-11-18 11:01:21 -0500

    for class

---
+++ 2024-11-18-advent2016/Tester.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Tester{
+  private int x,y;
+  public static void main(String[] args){
+    try{
+
+    }
+
+  }
+  public static int distance(String filename){
+    File input = new File(filename);
+    Scanner file = new Scanner(input);
+    int[] direction = {0,1,2,3}; // North 0, West 1, South 2, East 3;
+    while(fine.hasNext()){ // maybe try making an array instead using split(", "), change the direction array to go forward or backward depending on L/R at the input
+  }
+
+}
