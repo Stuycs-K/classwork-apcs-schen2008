2a1f42964d8ca60a7090d7a433ad72b576a6ed11
Sean Chen <schen200826@gmail.com>
2024-12-03 22:43:27 -0500

    played around with terminal stuff

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,10 +1,29 @@
 public class ColorDemo{
   public static void main(String[] args){
-    for(int i = 0; i < 257; i++){
-      int r = 0;
-      int b = 0;
-      int g = 0;
-      System.out.println("\u001b[" + r + ";" + g + ";" b + ";m");
+    int x = 31;
+    for(int i = 0; i < 7; i++){
+      System.out.print("\u001b[" + x + "m");
+      System.out.println("This is a different color from above");
+      x++;
+    }
+    x--;
+    for(int i = 0; i < 6; i++){
+      System.out.print("\u001b[" + (x+10) + "m");
+      System.out.print("diff");
+      x--;
+    }
+    System.out.print("\u001b[0m");
+    System.out.println("We are going back to normal");
+    System.out.print("\u001b[2J");
+    System.out.println("scroll up, screen was cleared");
+    for(int i = 0; i < 6; i++){
+      System.out.print("\u001b[" + (x+10) + "m");
+      System.out.print("\u001b[" + (x+10) + "7m");
+      System.out.print("reversing text to background");
+      System.out.println("\u001b[0m");
+      System.out.print("Back to normal again");
+
+      x++;
     }
   }
 }
