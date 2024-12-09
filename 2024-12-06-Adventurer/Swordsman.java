461fa7a62413629a59e86af002dd6d793b66e39f
Sean Chen <schen200826@gmail.com>
2024-12-08 22:13:53 -0500

    tested everything, also apparently something went wrong after I changed name to Swordsman.java, not sure if anything broken. fixed up special attack as well

--- 2024-12-06-Adventurer/Swordsman.java
+++ 2024-12-06-Adventurer/Swordsman.java
@@ -1,10 +1,54 @@
 import java.util.Random;
-public class Samurai implements Adventurer{
+public class Swordsman extends Adventurer{
+    private int qi;
+    private int maxQi;
     public Swordsman(String name, int hp) {
         super(name, hp);
     }
     public Swordsman(String name) {
-        this(name, 15);
+        super(name);
+    }
+    public String getSpecialName(){
+        return "qi";
+    }
+    public void setSpecial(int n){
+        qi = n;
+    }
+    public int getSpecialMax(){
+        return maxQi;
+    }
+    public int getSpecial(){
+        return qi;
+    }
+      //hurt or hinder the target adventurer
+    public String attack(Adventurer other){
+        other.applyDamage(5);
+        return getName() + " slashes at " + other.getName() + " for 5 damage!";
+    }
+
+    //heall or buff the target adventurer
+    public String support(Adventurer other){
+        other.setHP(other.getHP() + 3);
+        return getName() + " heals " + other.getName() + " for 3 health!";
+    }
+
+    //heall or buff self
+    public String support(){
+        setHP(this.getHP() + 3);
+        return getName() + " heals " + getName() + " for 3 health!";
+    }
+
+    //hurt or hinder the target adventurer, consume some special resource
+    public String specialAttack(Adventurer other){
+        if(qi-1 >= 0){
+            qi--;
+            other.applyDamage(8);
+            return getName() + " used Qi Sword on " + other.getName() + " for 8 damage!";
+        }
+        else{
+            return "No qi! Attack failed!";
+        }
+
     }
 
 }
