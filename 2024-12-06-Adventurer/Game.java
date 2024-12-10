d50f22d21ef05ea875d15b1e2e5d490ac8756b92
Sean Chen <schen200826@gmail.com>
2024-12-09 19:34:34 -0500

    wrote working game

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -8,23 +8,31 @@ public class Game{
     System.out.println("Username is: " + userName);
 
     //Creation of characters
-    Adventurer p1 = Swordsman(userName);
-    Adventurer p2 = CodeWarrior("APCS Goon");
+    Adventurer p1 = new Swordsman(userName);
+    Adventurer p2 = new CodeWarrior("APCS Goon");
 
     //Game code
     while(1 == 1){
       System.out.println("Player: " + p1.getName() + ", " + p1.getHP() + "/" + p1.getmaxHP() + " HP" + ", " + p1.getSpecial() + "/" + p1.getSpecialMax() + " " + p1.getSpecialName());
       System.out.println("Enemy: " + p2.getName() + ", " + p2.getHP() + "/" +  p2.getmaxHP() + " HP" + ", " + p2.getSpecial() + "/" + p2.getSpecialMax() + " " + p2.getSpecialName());
       System.out.println("Type:  (a)ttack / (sp)ecial / (su)pport / quit");
+      if(p1.getHP() <= 0){
+        System.out.println("APCSGoon has defeated " + p1.getName());
+        return;
+      }
+      else if (p2.getHP() <= 0){
+        System.out.println("You win!");
+        return;
+      }
         String action = userInput.nextLine();
         if(action.equals("a") || action.equals("attack")){
-          p1.attack(p2);
+          System.out.println(p1.attack(p2));
         }
         else if(action.equals("sp") || action.equals("special")){
-          p1.special(p2);
+          System.out.println(p1.specialAttack(p2));
         }
         else if(action.equals("su") || action.equals("support")){
-          p1.support();
+          System.out.println(p1.support());
         }
         else if(action.equals("quit")){
           System.out.println("User has ended simulation");
@@ -32,6 +40,24 @@ public class Game{
         }
         else{
           System.out.println("Please enter a valid action");
+        }
+        if(p1.getHP() <= 0){
+          System.out.println("APCSGoon has defeated " + p1.getName());
+          return;
+        }
+        else if (p2.getHP() <= 0){
+          System.out.println("You win!");
+          return;
+        }
+        int botaction = (int) Math.random()*2;
+        if(botaction == 0){
+          System.out.println(p2.attack(p1));
+        }
+        else if(botaction == 1){
+          System.out.println(p2.specialAttack(p1));
+        }
+        else if(botaction == 2){
+          System.out.println(p2.support());
         }
     }
   }
