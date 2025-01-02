a6f72d111096f1cf57ed8e64b1f4508e3f8f6ae2
Sean Chen <schen200826@gmail.com>
2025-01-02 10:59:23 -0500

    highest fib under 10s is 46

---
+++ 2025-01-02/Fibonacci.java
@@ -0,0 +1,20 @@
+public class Fibonacci{
+  public static int fib(int n){
+    int sum = 0;
+    if(n==1){
+      return 1;
+    }
+    else if(n == 0){
+      return 0;
+    }
+    else{
+      sum += fib(n-1) + fib(n-2);
+    }
+    return sum;
+  }
+  public static void main(String[] args){
+    if(args[1].equals("fib")){
+      fib(Integer.parseInt(args[0]));
+    }
+  }
+}
