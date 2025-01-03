c04245f1ce6ed5b4f5541ef9c6b4656268ee38a8
Sean Chen <schen200826@gmail.com>
2025-01-03 11:01:15 -0500

    saving for end bell

---
+++ 2025-01-02/makeWords.java
@@ -0,0 +1,13 @@
+public class makeWords{
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    if(remainingLetters == 0){
+      System.out.println(result);
+    }else{
+      if()
+      result += alphabet.substring(0,1);
+    }
+  }
+  public static void main(String[] args){
+
+  }
+}
