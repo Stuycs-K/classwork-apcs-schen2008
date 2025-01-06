64f35b3ace4ae7a33f4501612db15494adfb197d
Sean Chen <schen200826@gmail.com>
2025-01-05 22:01:02 -0500

    finished makeWords

--- 2025-01-02/makeWords.java
+++ 2025-01-02/makeWords.java
@@ -2,12 +2,17 @@ public class makeWords{
   public static void makeWords(int remainingLetters, String result, String alphabet){
     if(remainingLetters == 0){
       System.out.println(result);
-    }else{
-      if()
-      result += alphabet.substring(0,1);
+      return;
+    }else if(alphabet.equals("")){
+      return;
     }
+    for (int i = 0; i < alphabet.length(); i++) {
+      makeWords(remainingLetters - 1, result + alphabet.charAt(i), alphabet);
+  } 
   }
   public static void main(String[] args){
-
+    String alphabet = "abc"; 
+    int wordLength = 3;      
+    makeWords(wordLength, "", alphabet);
   }
 }
